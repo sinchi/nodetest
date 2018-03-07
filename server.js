0d6217fe9cae90e5a58e58586c013d10e9ad7bd8
ayoub <ayoub.belghar@gmail.com>
2018-03-07 21:01:01 +0000

    testo

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+// Initialize Express app
+var app = express();
+// Configure
+
+// log every request to the console
+app.use(morgan('dev'));
+// For parsing HTTP responses
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+let data = {
+  question: 'Aimez-vous Javascript ?',
+  reponses: {
+    oui: 'Bon choix !',
+    non:  'Zut !'
+  }
+};
+
+app.get('/api', function(req, res){
+  return res.status(200).send({ data: data.question });
+});
+
+app.post('/api', function(req, res){
+  let rep = req.body.response.toLowerCase() || null;
+  return res.status(201).send({data: rep !== null ? data.reponses[rep] : {} });
+})
+
+
+
+app.listen(3000);
